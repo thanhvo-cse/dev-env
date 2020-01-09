625c42e810556bde4fcc350e4b652f119a423b94
Thanh Vo <thanh.vo@legato.co>
2020-01-09 13:57:52 +0700

    Implement import sub command

--- src/base.ts
+++ src/base.ts
@@ -1,7 +1,42 @@
 import Command, {flags} from '@oclif/command'
+import * as Config from "@oclif/config"
+import Env from "./libs/env"
+import AppConfig from "./libs/appConfigs"
+import CustomConfig from "./libs/customConfig"
+import Shell from './libs/shell'
+import Files from "./libs/files";
 
 export default abstract class extends Command {
+    protected env: Env = new Env()
+    protected appConfig: AppConfig = new AppConfig()
+    protected customConfig: CustomConfig = new CustomConfig()
+    protected files: Files = new Files()
+    protected shell: Shell = new Shell()
+
+    static readonly WORKSPACE_DIR: string = 'WORKSPACE_DIR'
+    static readonly NETWORK: string = 'NETWORK'
+    static readonly PROJECT_NAME: string = 'PROJECT_NAME'
+
     static flags = {
         help: flags.help({char: 'h'})
+    }
+
+    constructor(argv: string[], config: Config.IConfig) {
+        super(argv, config)
+
+        this.env.set(Env.CONFIG_ROOT, config.root)
+        this.env.set(Env.CONFIG_DATA_DIR, config.dataDir)
+        this.env.set(Env.CONFIG_CONFIG_DIR, config.configDir)
+    }
+
+    async init() {
+        await this.env.set(Env.WORKSPACE_DIR, await this.customConfig.get(CustomConfig.WORKSPACE_DIR))
+
+        const network = await this.customConfig.get(CustomConfig.NETWORK)
+
+        if (network) {
+            let {stdout} = await this.shell.sh(`ipconfig getifaddr ${await this.customConfig.get(CustomConfig.NETWORK)}`)
+            await this.env.set(Env.REMOTE_HOST, stdout.trim())
+        }
     }
 }
---
+++ src/libs/files/googleDrive.ts
@@ -0,0 +1,142 @@
+import * as fs from 'fs'
+import {join} from 'path'
+import * as readline from 'readline'
+import {google} from 'googleapis'
+
+export class GoogleDrive {
+  // If modifying these scopes, delete token.json.
+  private readonly SCOPES = [
+      'https://www.googleapis.com/auth/drive',
+  ]
+
+  // The file token.json stores the user's access and refresh tokens, and is
+  // created automatically when the authorization flow completes for the first
+  // time.
+  private readonly TOKEN_PATH = 'token.json'
+
+  async traversal(folder: string, name: string, callback: any) {
+    this.find(folder, name,function(auth: any, folderItem: any) {
+      const drive = google.drive({version: 'v3', auth})
+      drive.files.list({
+        q: `parents in '${folderItem.id}'`,
+        fields: 'files(id, name)'
+      }, (err, res: any) => {
+        if (err) return console.log('The API returned an error: ' + err)
+        const files = res.data.files
+        if (files.length) {
+          console.log('Files:')
+          files.map((file: any) => {
+            console.log(`${file.name} (${file.id})`)
+            callback(auth, file)
+          })
+        } else {
+          console.log('No files found.')
+        }
+      })
+    })
+  }
+
+  async find(folder: string, name: string, callback: any) {
+    this.execute(function (auth: any) {
+      const drive = google.drive({version: 'v3', auth})
+      drive.files.list({
+        q: `parents in '${folder}' and name='${name}'`,
+        fields: 'files(id, name)',
+      }, (err, res: any) => {
+        if (err) return console.log('The API returned an error: ' + err)
+        const files = res.data.files
+        if (files.length) {
+          console.log(`${files[0].name} (${files[0].id})`)
+          callback(auth, files[0])
+        } else {
+          console.log('No files found.')
+        }
+      })
+    })
+  }
+
+  async download(folder: string, target: string, dest: string, callback: any) {
+    this.traversal(folder, target,function(auth: any, item: any) {
+      var destFile = fs.createWriteStream(join(dest, item.name))
+      const drive = google.drive({version: 'v3', auth})
+      drive.files.get({
+        fileId: item.id,
+        alt: 'media'
+      },
+          {responseType: 'stream'},
+          function(err, res: any){
+            res.data
+                .on('end', () => {
+                    console.log(`Download completed: ${target}`)
+                    callback()
+                  })
+                .on('error', (err: any) => {
+                    console.log('Error', err)
+                  })
+                // .pipe(destFile)
+          }
+      )
+    })
+  }
+
+  private async execute(callback: any) {
+    // Load client secrets from a local file.
+    fs.readFile(join(__dirname, './gdrive.json'), (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err)
+      // Authorize a client with credentials, then call the Google Drive API.
+      this.authorize(JSON.parse(content + ''), callback)
+    })
+
+  }
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  private async authorize(credentials: any, callback: any) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0])
+
+    // Check if we have previously stored a token.
+    fs.readFile(this.TOKEN_PATH, (err, token) => {
+      if (err) return this.getAccessToken(oAuth2Client, callback)
+      oAuth2Client.setCredentials(JSON.parse(token + ''))
+      callback(oAuth2Client)
+    })
+  }
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback for the authorized client.
+   */
+  private async getAccessToken(oAuth2Client: any, callback: any) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: this.SCOPES,
+    })
+    console.log('Authorize this app by visiting this url:', authUrl)
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close()
+      oAuth2Client.getToken(code, (err: any, token: string) => {
+        if (err) return console.error('Error retrieving access token', err)
+        oAuth2Client.setCredentials(token)
+        // Store the token to disk for later program executions
+        fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err)
+          console.log('Token stored to', this.TOKEN_PATH)
+        })
+        callback(oAuth2Client)
+      })
+    })
+  }
+
+}
