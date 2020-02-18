282ef4e469b626cfa3658e2f64f4572c464e638a
Quang Huy <quanghuy9490@gmail.com>
2020-02-18 23:19:26 +0700

    Create configure:network subcommand

---
+++ src/commands/configure/network.ts
@@ -0,0 +1,27 @@
+import Command from '../../base'
+import cli from 'cli-ux'
+import CustomConfig from "../../libs/customConfig"
+
+export default class Network extends Command {
+  static description = 'Set or show network name'
+
+  static args = [
+    {
+      name: 'network name',
+      required: false,
+      description: 'Set the network name'
+    }
+  ]
+
+  static flags = {
+    ...Command.flags
+  }
+
+  async run() {
+    if (this.args.command != undefined) {
+      await this.customConfig.set(CustomConfig.NETWORK, this.args.value)
+    } else {
+      console.log(await this.customConfig.get(CustomConfig.NETWORK))
+    }
+  }
+}
