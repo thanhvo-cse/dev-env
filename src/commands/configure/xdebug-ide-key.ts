30c73e8830fc19a2977bbead39c7805c340b1d6a
Quang Huy <quanghuy9490@gmail.com>
2020-02-22 18:49:01 +0700

    Implement configure:xdebug-ide-key subcommand.

---
+++ src/commands/configure/xdebug-ide-key.ts
@@ -0,0 +1,28 @@
+import Command from '../../base'
+import cli from 'cli-ux'
+import CustomConfig from "../../libs/customConfig"
+
+export default class XdebugIdeKey extends Command
+{
+  static description = 'Set and show Xdebug IDE Key'
+
+  static args = [
+    {
+      name: 'key',
+      required: false,
+      description: 'Set the Xdebug IDE Key'
+    }
+  ]
+
+  static flags = {
+    ...Command.flags
+  }
+
+  async run() {
+    if (this.args.command !== undefined) {
+      await this.customConfig.set(CustomConfig.XDEBUG_IDE_KEY, this.args.value)
+    } else {
+      console.log(await this.customConfig.get(CustomConfig.XDEBUG_IDE_KEY))
+    }
+  }
+}
