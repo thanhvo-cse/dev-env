7aa6b1a02e3d79a2e9b176b7f8f35dd3454960cc
Thanh Vo <thanhvo.cse@gmail.com>
2020-02-24 21:05:45 +0700

    fix #11

--- src/commands/mysql.ts
+++ src/commands/mysql.ts
@@ -2,6 +2,8 @@ import Command from '../base'
 import Const from './../const'
 import * as fs from "fs-extra"
 import {flags} from "@oclif/command"
+import {join} from "path"
+import {normalize} from "path"
 
 export default class Mysql extends Command {
   static description = 'Mysql'
@@ -47,18 +49,29 @@ export default class Mysql extends Command {
     const project = this.args[Const.ARG_PROJECT]
     const docker = await this.getDocker()
 
+    const dbBackupFile = await docker.getDbBackupFile(project)
+    const dbBackupFileBk = normalize(dbBackupFile) + '.bk'
+
+    if (this.args.file != undefined && fs.existsSync(dbBackupFile)) {
+      await fs.moveSync(dbBackupFile, dbBackupFileBk, {overwrite: true})
+    }
+
     if (this.args.command == 'backup') {
       await docker.dbBackup(project)
 
       if (this.args.file != undefined) {
-        await fs.copy(await docker.getDbBackupFile(project), this.args.file)
+        await fs.copySync(dbBackupFile, this.args.file, {overwrite: true})
       }
     } else if (this.args.command == 'restore') {
       if (this.args.file != undefined) {
-        await fs.copy(this.args.file, await docker.getDbBackupFile(project))
+        await fs.copySync(this.args.file, dbBackupFile, {overwrite: true})
       }
 
       await docker.dbRestore(project)
+    }
+
+    if (this.args.file != undefined && fs.existsSync(dbBackupFileBk)) {
+      await fs.moveSync(dbBackupFileBk, dbBackupFile, {overwrite: true})
     }
   }
 }
