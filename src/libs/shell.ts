2b4cc45b1254e27dc797b0603b10ca043be88d12
Huy Thai <NeoHuy@users.noreply.github.com>
2020-03-27 11:56:48 +0700

    Update option maxBuffer for shell (#15)

--- src/libs/shell.ts
+++ src/libs/shell.ts
@@ -1,9 +1,11 @@
 import {exec} from 'child_process'
 
 export default class Shell {
+  static readonly MAX_BUFFER = 1024 * 1024 //Default 200 * 1024 Byte
+
   async sh(cmd: string, quiteFlag: boolean = false): Promise<any> {
     return new Promise(function (resolve, reject) {
-      const makeProcess = exec(cmd, (err, stdout, stderr) => {
+      const makeProcess = exec(cmd, { maxBuffer: this.MAX_BUFFER }, (err, stdout, stderr) => {
         if (err) {
           reject(err)
         } else {
