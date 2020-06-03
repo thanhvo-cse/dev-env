56d47d57c99050bd5a328f6dfdb4385f31080d22
Sunny Phan <nhan.phan@legato.co>
2020-06-03 11:52:15 +0700

    Fix syntack error

--- src/base.ts
+++ src/base.ts
@@ -10,7 +10,7 @@ import DockerAbstract from "./services/dockerAbstract"
 import DockerSource from "./services/dockerSource"
 import DockerLocal from "./services/dockerLocal"
 import DockerUpstream from "./services/dockerUpstream"
-import * as updateNotifier from 'update-notifier';
+import { UpdateNotifier } from 'update-notifier';
 
 export default abstract class extends Command {
   protected env: Env = new Env()
@@ -39,9 +39,9 @@ export default abstract class extends Command {
     this.env.set(Env.DATA_LOCAL_DB_DIR, join(config.dataDir, Const.LOCAL_DB_DIR))
     this.env.set(Env.DATA_LOCAL_DB_BACKUP_DIR, join(config.dataDir, Const.LOCAL_DB_BACKUP_DIR))
 
-    const pkg = require(join(Env.CONFIG_ROOT, './package.json'));
-
-    updateNotifier({pkg}).notify();
+    const pkg = require(join(config.root, './package.json'));
+    const notifier = new UpdateNotifier({pkg});
+    notifier.notify();
   }
 
   async init() {
